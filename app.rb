ced0a42895f4e501f73f305f35982eb9cc97be8c
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-11 02:14:04 +0000

    created instance variables for weather and precipation variables that determine the playlist page

--- app.rb
+++ app.rb
@@ -6,12 +6,11 @@ get("/") do
 end
 
 get("/process_city") do
-  @user_input=params.fetch("city_input")
+  @user_input=params.fetch("city_input").downcase
 
    #location information
-   @location=params.fetch("umbrella_input").downcase
    access_gmaps_key = ENV.fetch("GMAPS_KEY")
-   @gmaps_location= HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location}&key=#{access_gmaps_key}")
+   @gmaps_location= HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{@user_input}&key=#{access_gmaps_key}")
    @location_data=JSON.parse(@gmaps_location)
    @location_hash=@location_data.fetch("results").at(0)
    @geometry_hash=@location_hash.fetch("geometry")
@@ -24,5 +23,17 @@ get("/process_city") do
    pirate_weather_data = JSON.parse(pirate_weather_url)
    weather_hash = pirate_weather_data.fetch("currently")
    @current_temp = weather_hash.fetch("temperature")
-  erb(:process_city)
+  
+
+   #summary information
+   summary_hash = pirate_weather_data.fetch("hourly")
+   @summary=summary_hash.fetch("summary")
+
+  #precipation
+  hourly_hash = pirate_weather_data.fetch("hourly")
+  hourly_data_array = hourly_hash.fetch("data") 
+  data_array_hash=hourly_data_array[0]
+  @precipitation=data_array_hash.fetch("precipType")
+
+   erb(:process_city)
 end
